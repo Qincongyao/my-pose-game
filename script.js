c31c9e397955841c488153bed3cd4e4dbea2c6e1
Qincongyao <qincongyao211@gmail.com>
2025-04-23 12:42:11 +0800

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 window.onload = () => {
-  const script1 = document.createElement("script");
-  script1.src = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.21.0"; // ✅ 正确写法
-  document.head.appendChild(script1);
-
-  const script2 = document.createElement("script");
-  script2.src = "https://cdn.jsdelivr.net/npm/@tensorflow-models/posenet";
-  document.head.appendChild(script2);
 
   script2.onload = () => {
     startPoseNet();
